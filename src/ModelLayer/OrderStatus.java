64664d8caf8d04a81f0a6caca03207141ebcaf29
alexrp <alex@lycus.org>
2012-05-30 15:24:35 +0200

    Remove some leftover code from OrderStatus.

--- src/ModelLayer/OrderStatus.java
+++ src/ModelLayer/OrderStatus.java
@@ -3,13 +3,11 @@ package ModelLayer;
 public class OrderStatus
 {
     private long _id;
-    //private Order _order;
     private String _statusValue;
 
     public OrderStatus(long id, String statusValue)
     {
         _id = id;
-        //_order = order;
         _statusValue = statusValue;
     }
 
@@ -17,16 +15,6 @@ public class OrderStatus
     {
         return _id;
     }
-
-/*    public Order getOrder()
-    {
-        return _order;
-    }
-
-    public void setOrder(Order order)
-    {
-        _order = order;
-    }*/
 
     public String getStatusValue()
     {
