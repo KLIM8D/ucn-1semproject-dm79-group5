719331cfe61155779de0547196da367abc74c65c
Christind <Christind@hotmail.com>
2012-06-01 10:56:25 +0200

    Made Person Controller, changed a few lines in rentalMenu & Personcontainer

---
+++ src/ControlLayer/PersonCtrl.java
@@ -0,0 +1,73 @@
+package ControlLayer;
+import ModelLayer.*;
+import java.util.ArrayList;
+
+/** 
+* @version: 0.1
+* Filename: PersonCtrl.java
+* Description: Controller class handling all operations with Persons
+* @changes    
+*/
+public class PersonCtrl
+{
+    private PersonContainer _personContainer;
+
+    /**
+     * Constructor for objects of class PersonCtrl
+     */
+    public PersonCtrl()
+    {
+        _personContainer = PersonContainer.getInstance();
+    }
+    
+    public boolean createPerson(long personId, String name, String address, String city, int zipCode, long phoneNumber)
+    {
+        Person per = new Person(personId, name, address, city, zipCode, phoneNumber);
+        return _personContainer.addPerson(per);
+    }
+    
+    public boolean updatePerson(long personId, String name, String address, String city, int zipCode, long phoneNumber)
+    {
+        Person per = _personContainer.getPerson(personId);
+        if(per != null)
+        {
+            per.setName(name);
+            per.setAddress(address);
+            per.setCity(city);
+            per.setZipCode(zipCode);
+            per.setPhoneNumber(phoneNumber);
+            return _personContainer.updatePerson(per);
+        }
+        return false;
+    }
+    
+    public boolean removePerson(long personId)
+    {
+        return _personContainer.removePerson(personId);
+    }
+    
+    public Person getPerson(long personId)
+    {
+        return _personContainer.getPerson(personId);
+    }
+    
+    public Person findPerson(String name)
+    {
+        return _personContainer.findPerson(name);
+    }
+    
+    public Person findPerson(long phoneNumber)
+    {
+        return _personContainer.findPerson(phoneNumber);
+    }
+    
+    public ArrayList<Person> getAllPersons()
+    {
+        ArrayList<Person> persons = new ArrayList<Person>();
+        for(Person person : _personContainer.getAllPersons().values())
+        {
+            persons.add(person);
+        }
+        return persons;
+    }
+}
--- src/TUILayer/CustomerMenuUI.java
+++ src/TUILayer/CustomerMenuUI.java
@@ -1,4 +1,8 @@
 package TUILayer;
+import ControlLayer.PersonCtrl;
+//import ControlLayer.CustomerCtrl;
+import ModelLayer.Customer;
+import ModelLayer.Person;
 
 /**
  * Text User Interface - Customer
@@ -8,72 +12,140 @@ package TUILayer;
 
 public class CustomerMenuUI
 {
-	private MainMenuUI _mainmenuUI;
+    private MainMenuUI _mainmenuUI;
+    private PersonCtrl _personCtrl;
+    //private CustomerCtrl _customerCtrl;
 
-	public void execCustomerMenu()
-	{
-		GlobalUI.tuiHeader();
+    public void execCustomerMenu()
+    {
+        GlobalUI.tuiHeader();
 
-		System.out.println("\n\n\n                    -------------------------------------------------------------------");
-		System.out.println("                    ¦                          Kundekartotek                          ¦");
-		System.out.println("                    -------------------------------------------------------------------");
-		System.out.println("                    ¦ 1) - Opret person                                               ¦");
-		System.out.println("                    ¦ 2) - Find person                                                ¦");
-		System.out.println("                    ¦ 3) - Slet person                                                ¦");
-		System.out.println("                    ¦                                                                 ¦");
-		System.out.println("                    ¦ 9) - Returner til hovedmenu                                     ¦");
-		System.out.println("                    -------------------------------------------------------------------");
-		
-		int userentry = GlobalUI.inputGetInt("\n                      Menu valg: ");
+        System.out.println("\n\n\n                    -------------------------------------------------------------------");
+        System.out.println("                    ¦                          Kundekartotek                          ¦");
+        System.out.println("                    -------------------------------------------------------------------");
+        System.out.println("                    ¦ 1) - Opret kunde                                                ¦");
+        System.out.println("                    ¦ 2) - Find kunde (Kundenummer)                                   ¦");
+        System.out.println("                    ¦ 3) - Slet kunde                                                 ¦");
+        System.out.println("                    ¦ 4) - Opdater kunde                                              ¦");
+        System.out.println("                    ¦ 5) - Vis Alle kunder                                            ¦");
+        System.out.println("                    ¦ 6) - Vis Business kunder                                        ¦");
+        System.out.println("                    ¦ 7) - Vis Private kunder                                         ¦");
+        System.out.println("                    ¦                                                                 ¦");
+        System.out.println("                    ¦ 0) - Returner til hovedmenu                                     ¦");
+        System.out.println("                    -------------------------------------------------------------------");
+        
+        int userentry = GlobalUI.inputGetInt("\n                      Menu valg: ");
 
-		try
-		{
-			switch (userentry)
-			{
-				case 1:
-					// Removal of section when function is integrated
-					// Start of section
-					System.out.print("\n                      " + GlobalUI.errorHandling(03));
-					Thread.sleep(2000);
-					execCustomerMenu();
-					break;
-					// End of section
+        try
+        {
+            switch (userentry)
+            {
+                case 1:
+                    // Removal of section when function is integrated
+                    // Start of section
+                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
+                    Thread.sleep(2000);
+                    execCustomerMenu();
+                    break;
+                    // End of section
 
-				case 2:
-					// Removal of section when function is integrated
-					// Start of section
-					System.out.print("\n                      " + GlobalUI.errorHandling(03));
-					Thread.sleep(2000);
-					execCustomerMenu();
-					break;
-					// End of section
+                case 2:
+                    // Removal of section when function is integrated
+                    // Start of section
+                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
+                    Thread.sleep(2000);
+                    execCustomerMenu();
+                    break;
+                    // End of section
 
-				case 3:
-					// Removal of section when function is integrated
-					// Start of section
-					System.out.print("\n                      " + GlobalUI.errorHandling(03));
-					Thread.sleep(2000);
-					execCustomerMenu();
-					break;
-					// End of section
+                case 3:
+                //                     try{
+                //                         long customerId = GlobalUI.inputGetLong("Indtast Kundenummer: ");
+                //                         Person per = _customerCtrl.getCustomer(customerId).getPerson();
+                //                         if(per != null)
+                //                         {
+                //                             long personId = per.getPersonId();
+                //                             boolean done = _customerCtrl.removePerson(personId);
+                //                             boolean done2 = _customerCtrl.removeCustomer(customerId);
+                //                             if(!done || !done2)
+                //                             {
+                //                                 System.out.println(GlobalUI.errorHandling(99));
+                //                                 GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                //                             }
+                //                             else
+                //                             {
+                //                                 System.out.println("Kunden er blevet slettet fra systemet!");
+                //                                 GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                //                             }
+                //                         }
+                //                         else
+                //                         {
+                //                             System.out.println("Personen eksisterer ikke i person kardoteket!");
+                //                             GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                //                         }
+                //                     }
+                //                     catch(Exception ex)
+                //                     {
+                //                         System.out.println("*FEJL* Kundenummer bestaar kun af tal");
+                //                         GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                //                     }
+                                         
+                     execCustomerMenu();
+                     break;
+                    
+                case 4:
+                    // Removal of section when function is integrated
+                    // Start of section
+                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
+                    Thread.sleep(2000);
+                    execCustomerMenu();
+                    break;
+                    // End of section
 
-				case 9:
-					_mainmenuUI = new MainMenuUI();
-					_mainmenuUI.execMainMenu();
-					break;
+                case 5:
+                    // Removal of section when function is integrated
+                    // Start of section
+                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
+                    Thread.sleep(2000);
+                    execCustomerMenu();
+                    break;
+                    // End of section
 
-				default:
-					System.out.print("\n                      " + GlobalUI.errorHandling(02));
-					Thread.sleep(2000);
-					execCustomerMenu();
-					break;
-			}
+                case 6:
+                    // Removal of section when function is integrated
+                    // Start of section
+                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
+                    Thread.sleep(2000);
+                    execCustomerMenu();
+                    break;
+                    // End of section
+                    
+                case 7:
+                    // Removal of section when function is integrated
+                    // Start of section
+                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
+                    Thread.sleep(2000);
+                    execCustomerMenu();
+                    break;
+                    // End of section
+                    
+                case 0:
+                    _mainmenuUI = new MainMenuUI();
+                    _mainmenuUI.execMainMenu();
+                    break;
 
-		}
+                default:
+                    System.out.print("\n                      " + GlobalUI.errorHandling(02));
+                    Thread.sleep(2000);
+                    execCustomerMenu();
+                    break;
+            }
 
-		catch (Exception e)
-		{
-			System.out.print("\n                      " + GlobalUI.errorHandling(99));
-		}
-	}
+        }
+
+        catch (Exception e)
+        {
+            System.out.print("\n                      " + GlobalUI.errorHandling(99));
+        }
+    }
 }
