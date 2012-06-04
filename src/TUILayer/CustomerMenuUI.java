4b2881b387101bbef543b1152bd52c08d0222b1a
Christind <Christind@hotmail.com>
2012-06-04 10:08:54 +0200

    Added methods to CustomerUI

--- src/TUILayer/CustomerMenuUI.java
+++ src/TUILayer/CustomerMenuUI.java
@@ -1,6 +1,6 @@
 package TUILayer;
 import ControlLayer.PersonCtrl;
-//import ControlLayer.CustomerCtrl;
+import ControlLayer.CustomerCtrl;
 import ModelLayer.Customer;
 import ModelLayer.Person;
 
@@ -14,7 +14,7 @@ public class CustomerMenuUI
 {
     private MainMenuUI _mainmenuUI;
     private PersonCtrl _personCtrl;
-    //private CustomerCtrl _customerCtrl;
+    private CustomerCtrl _customerCtrl;
 
     public void execCustomerMenu()
     {
@@ -41,66 +41,148 @@ public class CustomerMenuUI
             switch (userentry)
             {
                 case 1:
-                    // Removal of section when function is integrated
-                    // Start of section
-                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
-                    Thread.sleep(2000);
+                    try{
+						long personId = GlobalUI.inputGetLong("Indtast CPR nummer (uden -): ");
+						String personName = GlobalUI.inputGetLine("Indtast navn: ");
+						String address = GlobalUI.inputGetLine("Indtast vejnavn + husnummer: ");
+						int zipCode = GlobalUI.inputGetInt("Indtast post nummer: ");
+						String city = GlobalUI.inputGetLine("Indtast by: ");						
+						long phoneNumber = GlobalUI.inputGetLong("Indtast telefon nummer: ");
+						boolean done = _personCtrl.createPerson(personId, personName, address, city, zipCode, phoneNumber);
+                        if(done)
+                        {
+                            String isBusiness = GlobalUI.inputGetLine("Er der tale om en erhvervskunde? (ja/nej): ");
+                            if(isBusiness.toLowerCase().equals("ja"))
+                            {
+                                String contactPerson =  GlobalUI.inputGetLine("Indtast kontaktperson navn: ");
+                                long cvrNo =  GlobalUI.inputGetLong("Indtast virksomhedens CVR-nummer: ");
+                                Person per = _personCtrl.getPerson(personId);
+                                _customerCtrl.createCustomer(per);
+                            }
+                            else if(isBusiness.toLowerCase().equals("nej"))
+                            {
+                                Person per = _personCtrl.getPerson(personId);
+                                _customerCtrl.createCustomer(per);
+                            }
+                            else
+                            {
+                                System.out.println("Der skete en fejl under indtastningen af kundens erhvervsoplysninger");
+                            }
+                        }
+                        else
+                            System.out.println("Der skete en fejl under indtastningen af kundens informationer");
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.println("*FEJL* Kundenummer bestaar kun af tal");
+                        GlobalUI.inputGetLine("Tryk enter for at fortsaette..");                       
+                    }
+                
                     execCustomerMenu();
                     break;
-                    // End of section
 
                 case 2:
-                    // Removal of section when function is integrated
-                    // Start of section
-                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
-                    Thread.sleep(2000);
+                    long customerId = 0;
+                    try{
+                        customerId = GlobalUI.inputGetLong("Indtast Kundenummer: ");
+                        Customer customer = _customerCtrl.getCustomer(customerId);
+                        if(customer == null)
+                        {
+                            System.out.println(GlobalUI.errorHandling(99));
+                            GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                        }
+                        else
+                        {
+                            System.out.println(GlobalUI.getCustomerInfo(customer));
+                            GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.println("*FEJL* UdlejningsID bestaar kun af tal");
+                        GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                    }
                     execCustomerMenu();
                     break;
                     // End of section
 
                 case 3:
-                //                     try{
-                //                         long customerId = GlobalUI.inputGetLong("Indtast Kundenummer: ");
-                //                         Person per = _customerCtrl.getCustomer(customerId).getPerson();
-                //                         if(per != null)
-                //                         {
-                //                             long personId = per.getPersonId();
-                //                             boolean done = _customerCtrl.removePerson(personId);
-                //                             boolean done2 = _customerCtrl.removeCustomer(customerId);
-                //                             if(!done || !done2)
-                //                             {
-                //                                 System.out.println(GlobalUI.errorHandling(99));
-                //                                 GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
-                //                             }
-                //                             else
-                //                             {
-                //                                 System.out.println("Kunden er blevet slettet fra systemet!");
-                //                                 GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
-                //                             }
-                //                         }
-                //                         else
-                //                         {
-                //                             System.out.println("Personen eksisterer ikke i person kardoteket!");
-                //                             GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
-                //                         }
-                //                     }
-                //                     catch(Exception ex)
-                //                     {
-                //                         System.out.println("*FEJL* Kundenummer bestaar kun af tal");
-                //                         GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
-                //                     }
+                    try{
+                        customerId = GlobalUI.inputGetLong("Indtast Kundenummer: ");
+                        Person per = _customerCtrl.getCustomer(customerId).getPerson();
+                        if(per != null)
+                        {
+                            long personId = per.getPersonId();
+                            boolean done = _personCtrl.removePerson(personId);
+                            boolean done2 = _customerCtrl.removeCustomer(customerId);
+                            if(!done || !done2)
+                            {
+                                System.out.println(GlobalUI.errorHandling(99));
+                                GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                            }
+                            else
+                            {
+                                System.out.println("Kunden er blevet slettet fra systemet!");
+                                GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                            }
+                        }
+                        else
+                        {
+                            System.out.println("Personen eksisterer ikke i person kardoteket!");
+                            GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.println("*FEJL* Kundenummer bestaar kun af tal");
+                        GlobalUI.inputGetLine("Tryk enter for at fortsaette..");
+                    }
                                          
                      execCustomerMenu();
                      break;
                     
                 case 4:
-                    // Removal of section when function is integrated
-                    // Start of section
-                    System.out.print("\n                      " + GlobalUI.errorHandling(03));
-                    Thread.sleep(2000);
-                    execCustomerMenu();
-                    break;
-                    // End of section
+                    try
+					{
+						String contact = "";
+						long cvrNo = 0;
+						boolean done2 = true;
+					    customerId = GlobalUI.inputGetLong("Indtast ID på lageret: ");
+						Customer customer = _customerCtrl.getCustomer(customerId);
+						if(customer != null)
+						{
+							 String cusName = GlobalUI.inputGetLine("Indtast Kundens navn: ");
+							 String cusAddress = GlobalUI.inputGetLine("Indtast Kundens addresse: ");;
+							 String cusCity = GlobalUI.inputGetLine("Indtast By: ");
+							 int cusZipCode = GlobalUI.inputGetInt("Indtast Postnummer: ");
+							 long cusPhoneNumber = GlobalUI.inputGetLong("Indtast Telefonnummer: ");
+							 if(customer.getIsBusiness())
+							 {
+							     contact = GlobalUI.inputGetLine("Indtast Kontaktperson: ");
+							     cvrNo = GlobalUI.inputGetLong("Indtast CVR-nummer: ");
+							     done2 = _customerCtrl.updateCustomer(customerId, contact, cvrNo);
+							 }
+							 boolean done = _personCtrl.updatePerson(customerId, cusName, cusAddress, cusCity, cusZipCode, cusPhoneNumber);
+							 if(done && done2)
+							 {
+							     System.out.println("Kunden er nu opdateret");
+							 }
+							 else
+							 {
+							     System.out.println("*FEJL* - Kunden blev ikke opdateret ");
+							 }
+						}	
+						else
+							System.out.println("En kunde med det ID blev ikke fundet");
+					}
+					catch(Exception ex)
+					{
+						System.out.println(GlobalUI.errorHandling(99));
+						GlobalUI.inputGetLine("Tryk på enter for at forsætte..");
+					}			
+	
+					execCustomerMenu();
+					break;
 
                 case 5:
                     // Removal of section when function is integrated
