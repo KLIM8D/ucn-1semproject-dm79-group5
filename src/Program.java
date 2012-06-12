5f6eaec49ddc572d70179c01acf258b04c3e9219
Christind <Christind@hotmail.com>
2012-06-12 12:26:27 +0200

    Changed

--- src/Program.java
+++ src/Program.java
@@ -7,11 +7,11 @@ final class Program
     public static void main(String[] args)
     {
     	// Text interface
-        TestData test = new TestData();
-        test.addData();
-    	new TUILayer.GlobalUI().mainRun();
+        //TestData test = new TestData();
+        //test.addData();
+    	//new TUILayer.GlobalUI().mainRun();
     	
     	// Graphic interface
-    	//new GUILayer.GlobalUI();
+    	new GUILayer.GlobalUI();
     }
 }
