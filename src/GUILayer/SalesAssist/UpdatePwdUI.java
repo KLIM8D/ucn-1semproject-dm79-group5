9cf6373e85a42433534b26be14c30b1ee0396c60
iammadsnielsen <contact@madsnielsen.net>
2012-06-14 15:44:33 +0200

    Method added, done....

--- src/GUILayer/SalesAssist/UpdatePwdUI.java
+++ src/GUILayer/SalesAssist/UpdatePwdUI.java
@@ -1,5 +1,6 @@
 package GUILayer.SalesAssist;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -9,18 +10,29 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 
+import ControlLayer.SalesAssistantCtrl;
+import ModelLayer.SalesAssistant;
+import GUILayer.GlobalUI;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class UpdatePwdUI extends JFrame {
 
+	private SalesAssistantCtrl _saController;
 	private static final long serialVersionUID = -2198226493427431793L;
+	protected static final Component frame = null;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JTextField txtSalesAssistId;
+	private JPasswordField txtPassword;
 
 	public UpdatePwdUI() {
+		_saController = new SalesAssistantCtrl();
+		
 		setTitle("Skift adgangskode");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(0, 0, 450, 167);
@@ -34,22 +46,30 @@ public class UpdatePwdUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel label = new JLabel("Ekspedient Id");
-		label.setBounds(12, 12, 121, 15);
-		contentPane.add(label);
+		JLabel lblSalesAssistId = new JLabel("Ekspedient Id");
+		lblSalesAssistId.setBounds(12, 12, 121, 15);
+		contentPane.add(lblSalesAssistId);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(137, 10, 299, 19);
-		contentPane.add(textField);
+		txtSalesAssistId = new JTextField();
+		txtSalesAssistId.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				if(txtSalesAssistId.getText().length() > 0)
+				{				
+					GlobalUI.checkIfInt(txtSalesAssistId);				
+				}
+			}
+		});
+		txtSalesAssistId.setColumns(10);
+		txtSalesAssistId.setBounds(137, 10, 299, 19);
+		contentPane.add(txtSalesAssistId);
 		
-		JLabel lblNyAdgangskode = new JLabel("Ny adgangskode");
-		lblNyAdgangskode.setBounds(12, 39, 132, 15);
-		contentPane.add(lblNyAdgangskode);
+		JLabel lblPassword = new JLabel("Ny adgangskode");
+		lblPassword.setBounds(12, 39, 132, 15);
+		contentPane.add(lblPassword);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(137, 37, 299, 19);
-		contentPane.add(passwordField);
+		txtPassword = new JPasswordField();
+		txtPassword.setBounds(137, 37, 299, 19);
+		contentPane.add(txtPassword);
 		
 		JButton btnChange = new JButton("Skift");
 		btnChange.addActionListener(new ActionListener() {
@@ -78,7 +98,33 @@ public class UpdatePwdUI extends JFrame {
 	}
 	
 	private void changePassword() {
-		
+		if(txtSalesAssistId.getText().length() == 0) {
+			JOptionPane.showMessageDialog(frame, GlobalUI.messageHandling(20), "FEJL!", JOptionPane.WARNING_MESSAGE);
+		}
+		else {
+			if(txtPassword.getPassword().toString().length() == 0) {
+				JOptionPane.showMessageDialog(frame, GlobalUI.messageHandling(21), "FEJL!", JOptionPane.WARNING_MESSAGE);
+			}
+			else {
+				try {
+					int salesAsstId = 0;
+					SalesAssistant sa = null;
+					
+					salesAsstId = Integer.parseInt(txtSalesAssistId.getText());
+					sa = _saController.getSalesAssistant(salesAsstId);
+					if(sa != null) {
+						_saController.changePassword(salesAsstId, txtPassword.getPassword().toString());
+						setVisible(false);
+						GUILayer.GlobalUI.setWindowStatus(false);
+					}
+					else{
+						JOptionPane.showMessageDialog(frame, GlobalUI.messageHandling(18), "FEJL!", JOptionPane.WARNING_MESSAGE);
+					}
+				}
+				catch (Exception e) {
+					JOptionPane.showMessageDialog(frame, GlobalUI.messageHandling(99), "FEJL!", JOptionPane.WARNING_MESSAGE);
+				}
+			}
+		}
 	}
-
 }
