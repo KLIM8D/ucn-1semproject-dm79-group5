a11f047b6aa023f92cc0f6eee596355231986c6f
iammadsnielsen <contact@madsnielsen.net>
2012-06-18 19:58:42 +0200

    Method done....

--- src/GUILayer/GlobalUI.java
+++ src/GUILayer/GlobalUI.java
@@ -10,7 +10,6 @@ public class GlobalUI {
 	private static String _systemTitle = "1. Semesters Projekt";
 	private static String _systemDescription = "UCN-DM79, Gruppe 5";
 	private static String _systemBuild = "build31052012";	
-	//private static boolean _isActive = false;
 	
 	public GlobalUI() {
 		try {
@@ -79,14 +78,6 @@ public class GlobalUI {
 		}
 		return null;
 	}
-	
-//	public static boolean getWindowStatus() {
-//		return _isActive;
-//	}
-//	
-//	public static void setWindowStatus(boolean status) {
-//		_isActive = status;
-//	}
 	
 	public static void checkIfInt(JTextField data) {
 		try {
